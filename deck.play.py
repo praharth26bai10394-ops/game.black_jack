aedf31e079daba02de99b64f78dc91792594c2aa
praharth26bai10394-ops <praharth.26bai10394@vitbhopal>
2026-09-21 16:12:21 +0530

    Updated deck module

--- deck.play.py
+++ deck.play.py
@@ -10,7 +10,6 @@ class Deck:
         suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
         ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10","J", "Q", "K", "A"]
                  
-
         for suit in suits:
             for rank in ranks:
                 self.cards.append(Card(suit, rank))
@@ -21,11 +20,13 @@ class Deck:
     def deal_card(self):
         return self.cards.pop()
 
-deck = Deck()
+# testing the game module
 
-print("Number of cards:", len(deck.cards))
+# deck = Deck()
 
-deck.shuffle()
+# print("Number of cards:", len(deck.cards))
 
-print("First card:", deck.deal_card())
-print("Cards remaining:", len(deck.cards))
+# deck.shuffle()
+
+# print("First card:", deck.deal_card())
+# print("Cards remaining:", len(deck.cards))
